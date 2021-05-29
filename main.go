9abeaa411abb23bc697d72691fc90554c0841384
Andrew Burke <atburke1985@gmail.com>
2021-05-28 19:50:42 -0500

    Add React

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "net/http"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func ping(c *gin.Context) {
-  c.String(http.StatusOK, "pong")
+	c.String(http.StatusOK, "pong")
 }
 
 func setupRouter() *gin.Engine {
-  router := gin.Default()
-  router.GET("/ping", ping)
-  return router
+	router := gin.Default()
+	router.GET("/ping", ping)
+	router.Static("/static", "./web/static")
+	rootLevelFiles := []string{"asset-manifest.json", "favicon.ico", "index.html", "logo192.png", "logo512.png", "manifest.json", "robots.txt"}
+	for _, file := range rootLevelFiles {
+		router.StaticFile(fmt.Sprintf("/%s", file), fmt.Sprintf("./web/%s", file))
+	}
+	router.StaticFile("/", "./web/index.html")
+	return router
 }
 
 func main() {
-  router := setupRouter()
-  router.Run()
+	router := setupRouter()
+	router.Run()
 }
--- main_test.go
+++ main_test.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
-  "net/http"
-  "net/http/httptest"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestPing(t *testing.T) {
-  router := setupRouter()
-  w := httptest.NewRecorder()
-  request := httptest.NewRequest("GET", "/ping", nil)
-  router.ServeHTTP(w, request)
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, request)
 
-  assert.Equal(t, http.StatusOK, w.Code)
-  assert.Equal(t, "pong", w.Body.String())
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
 }
