850fab6e3f02a24139bd7549dd1abbdb10382439
Andrew Burke <atburke1985@gmail.com>
2021-05-30 17:57:21 -0500

    Add frontend tests

---
+++ frontend/src/Dashboard.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('navigates on successful logout', async () => {
+  const logout = async () => true;
+  const navigate = jest.fn(() => {});
+
+  render(<Dashboard logout={logout} navigate={navigate} />);
+
+  const submit = await screen.findByText('Logout');
+  fireEvent.click(submit);
+
+  await waitFor(() => expect(navigate).toHaveBeenCalledWith('/login'));
+});
---
+++ frontend/src/Login.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import Login from './Login';
+
+test('navigates on successful login', async () => {
+  const login = async () => true;
+  const navigate = jest.fn(() => {});
+
+  render(<Login login={login} navigate={navigate} />);
+
+  const username = await screen.findByLabelText('Email Address');
+  const password = await screen.findByLabelText('Password');
+  const submit = await screen.findByText('Login to my Dashboard');
+
+  fireEvent.change(username, { target: { value: 'me@example.com' } });
+  fireEvent.change(password, { target: { value: 'topsneaky' } });
+  fireEvent.click(submit);
+
+  await waitFor(() => expect(navigate).toHaveBeenCalledWith('/dashboard'));
+  expect(() => screen.getByText('Invalid email/password.')).toThrow();
+});
+
+test('shows error message on failed login', async () => {
+  const login = async () => false;
+  const navigate = jest.fn(() => {});
+
+  render(<Login login={login} navigate={navigate} />);
+
+  const username = await screen.findByLabelText('Email Address');
+  const password = await screen.findByLabelText('Password');
+  const submit = await screen.findByText('Login to my Dashboard');
+
+  fireEvent.change(username, { target: { value: 'me@example.com' } });
+  fireEvent.change(password, { target: { value: 'topsneaky' } });
+  fireEvent.click(submit);
+
+  await waitFor(() => expect(screen.getByText('Invalid email/password.')).toBeTruthy());
+  expect(navigate).not.toHaveBeenCalledWith('/dashboard');
+});
