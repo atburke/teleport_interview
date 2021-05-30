7ce8117b84eb91efa2f28427e47d51d120e9f403
Andrew Burke <atburke1985@gmail.com>
2021-05-29 21:06:54 -0500

    Add CSRF token injection into index.html

--- main.go
+++ main.go
@@ -5,25 +5,114 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
-func ping(c *gin.Context) {
+// Env defines routes, as well as objects that should be shared across requests
+// (database connections, config, etc.)
+//
+// Defining routes like this allows for easy dependency injection in tests.
+type Env struct {
+	db Database
+}
+
+func (env *Env) ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
 
-func setupRouter() *gin.Engine {
+func (env *Env) serveIndex(c *gin.Context) {
+	now := time.Now()
+	indexData, err := os.ReadFile("./web/index.html")
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	csrfToken, err := GenerateToken()
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	session, err := env.db.CreatePreAuthSession(csrfToken, now)
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	// could consider templating this properly
+	newIndexData := strings.Replace(
+		string(indexData),
+		"window.csrfToken = '';",
+		fmt.Sprintf("window.csrfToken = '%s';", csrfToken),
+		1,
+	)
+
+	c.Header("Content-Type", "text/html")
+	sessionCookie := http.Cookie{
+		Name:     "session_token",
+		Value:    session.SessionToken,
+		Secure:   true,
+		HttpOnly: true,
+		// Same-site is lax by default
+	}
+
+	// Gin has a slightly weirder interface for this for some reason
+	http.SetCookie(c.Writer, &sessionCookie)
+	c.String(http.StatusOK, newIndexData)
+}
+
+func setupRouter(env *Env) *gin.Engine {
 	router := gin.Default()
-	router.GET("/ping", ping)
+	router.GET("/ping", env.ping)
+	router.GET("/index.html", env.serveIndex)
+	router.GET("/", env.serveIndex)
+
 	router.Static("/static", "./web/static")
-	rootLevelFiles := []string{"asset-manifest.json", "favicon.ico", "index.html", "logo192.png", "logo512.png", "manifest.json", "robots.txt"}
+	rootLevelFiles := []string{"asset-manifest.json", "favicon.ico", "logo192.png", "logo512.png", "manifest.json", "robots.txt"}
 	for _, file := range rootLevelFiles {
 		router.StaticFile(fmt.Sprintf("/%s", file), fmt.Sprintf("./web/%s", file))
 	}
-	router.StaticFile("/", "./web/index.html")
+
 	return router
 }
 
+func getEnvironment() (*Env, error) {
+	dbUsername, err := os.ReadFile("/run/secrets/mysql_user")
+	if err != nil {
+		return nil, err
+	}
+
+	dbPassword, err := os.ReadFile("/run/secrets/mysql_pw")
+	if err != nil {
+		return nil, err
+	}
+
+	dbName, err := os.ReadFile("/run/secrets/mysql_db")
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := NewMySqlDatabase(string(dbUsername), string(dbPassword), string(dbName))
+	if err != nil {
+		return nil, err
+	}
+	return &Env{db}, nil
+}
+
 func main() {
-	router := setupRouter()
-	log.Fatal(router.RunTLS(":8080", "/run/secrets/server-cert.pem", "/run/secrets/server-key.pem"))
+	env, err := getEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := setupRouter(env)
+	log.Fatal(router.RunTLS(
+		":8080", "/run/secrets/server-cert.pem", "/run/secrets/server-key.pem",
+	))
 }
--- main_test.go
+++ main_test.go
@@ -2,17 +2,72 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
+	"time"
 )
 
+// TODO: see if there's a better home for MockDatabase
+type MockDatabase struct {
+	session *Session
+	account *Account
+	error   error
+}
+
+func (m *MockDatabase) CreatePreAuthSession(csrfToken string, initTime time.Time) (*Session, error) {
+	return m.session, m.error
+}
+
+func (m *MockDatabase) CreateSession(accountId, csrfToken string, initTime time.Time) (*Session, error) {
+	return m.session, m.error
+}
+
+func (m *MockDatabase) FetchSession(sessionToken string) (*Session, error) {
+	return m.session, m.error
+}
+
+func (m *MockDatabase) DeleteSession(sessionToken string) error {
+	return m.error
+}
+
+func (m *MockDatabase) FetchAccount(email string) (*Account, error) {
+	return m.account, m.error
+}
+
+func (m *MockDatabase) DeleteExpiredSessions(t time.Time) error {
+	return m.error
+}
+
+func (m *MockDatabase) Close() {}
+
 func TestPing(t *testing.T) {
-	router := setupRouter()
+	router := setupRouter(&Env{&MockDatabase{}})
 	w := httptest.NewRecorder()
 	request := httptest.NewRequest("GET", "/ping", nil)
 	router.ServeHTTP(w, request)
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestIndex(t *testing.T) {
+	fakeToken := "00112233445566778899aabbccddeeff"
+	db := MockDatabase{session: &Session{SessionToken: fakeToken}}
+	env := &Env{&db}
+	router := setupRouter(env)
+	w := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/index.html", nil)
+	router.ServeHTTP(w, request)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Regexp(t, regexp.MustCompile(`window\.csrfToken = '\w{32}';`), w.Body.String())
+
+	cookies := w.Result().Cookies()
+	require.Equal(t, 1, len(cookies))
+	sessionCookie := cookies[0]
+	assert.Equal(t, "session_token", sessionCookie.Name)
+	assert.Equal(t, fakeToken, sessionCookie.Value)
 }
